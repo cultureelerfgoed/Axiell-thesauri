7b2c7e04722269f5c86401965ac3ab7f2333a339
RubenSchalk <rubenschalk1@gmail.com>
2023-09-29 16:37:17 +0200

    Update axiell_thesauri_analysis.R

--- axiell_thesauri_analysis.R
+++ axiell_thesauri_analysis.R
@@ -4,24 +4,28 @@
 library(stringdist)
 library(dplyr)
 library(data.table)
+library(foreach)
+library(doParallel)
 
 setwd("C:\\Users\\Ruben\\Documents\\05. RCE\\Axiell thesauri")
 
 # Sample list of concepts
 thesaurus <- fread("Thesarus-export(B&AC).csv")
 setDT(thesaurus)
-concepts <- thesaurus$term
+x <- thesaurus[sample(nrow(thesaurus), 10000), ]
+concepts <- x$term
 
-# Function to find similar concepts
-find_similar_concepts <- function(concepts, threshold = 0.75) {
+
+# Function to find similar concepts for a subset of concepts
+find_similar_concepts_subset <- function(subset_concepts, all_concepts, threshold = 0.75) {
   # Initialize an empty data frame to store results
   similar_concepts_df <- data.frame(concept1 = character(0), concept2 = character(0), similarity = numeric(0), stringsAsFactors = FALSE)
   
-  # Iterate through each pair of concepts
-  for (i in 1:length(concepts)) {
-    for (j in (i+1):length(concepts)) {
-      concept1 <- concepts[i]
-      concept2 <- concepts[j]
+  # Iterate through each pair of concepts in the subset
+  for (i in 1:length(subset_concepts)) {
+    for (j in seq_along(all_concepts)) {
+      concept1 <- subset_concepts[i]
+      concept2 <- all_concepts[j]
       
       # Split compounded concepts into individual tokens
       tokens1 <- unlist(strsplit(concept1, " "))
@@ -35,7 +39,8 @@ find_similar_concepts <- function(concepts, threshold = 0.75) {
       
       # If the average similarity is above the threshold and not NA, consider them similar
       if (!is.na(avg_similarity) && avg_similarity >= threshold) {
-        similar_concepts_df <- bind_rows(similar_concepts_df, data.frame(concept1 = concept1, concept2 = concept2, similarity = avg_similarity))
+        # Accumulate the results in a list
+        similar_concepts_df <- list(similar_concepts_df, data.frame(concept1 = concept1, concept2 = concept2, similarity = avg_similarity))
       }
     }
   }
@@ -43,11 +48,21 @@ find_similar_concepts <- function(concepts, threshold = 0.75) {
   return(similar_concepts_df)
 }
 
-# Find similar concepts with a threshold of 0.75
-similar_concepts <- find_similar_concepts(concepts, threshold = 0.75)
+# Set up parallel processing
+num_cores <- 4  # You can adjust this to the number of cores you want to use
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
 
-# Print the result
-print(similar_concepts)
+# Find similar concepts with a threshold of 0.75 in parallel
+similar_concepts_list <- foreach(subset = concepts) %dopar% {
+  find_similar_concepts_subset(subset, concepts, threshold = 0.75)
+}
+
+# Stop the parallel cluster
+stopCluster(cl)
+
+# Combine the results from the list
+similar_concepts <- do.call(rbind, similar_concepts_list)
 
 # Identify unique concepts and add a 'unique' variable
 thesaurus$unique <- !thesaurus$term %in% unique(c(similar_concepts$concept1, similar_concepts$concept2))
